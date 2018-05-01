53a1a9e299e38945395325fe57a6ecc5d26cc3db
Queen Adydams <adydams@gmail.com>
2018-05-01 11:32:26 -0700

    group migration

---
+++ server/migrations/20180413133235-create-group.js
@@ -0,0 +1,35 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Groups', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      groupname: {
+        type: Sequelize.STRING
+      },
+      description: {
+        type: Sequelize.STRING
+      },
+      creatorid: {
+        type: Sequelize.INTEGER,
+        
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Groups');
+  }
+};
